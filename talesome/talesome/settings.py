6a8f9574a911c2db4d72c03babd9088eeea16e53
Ashwin Joseph <ashwin.joseph@qburst.com>
2025-04-14 11:49:26 +0530

    Add debugging code

--- talesome/talesome/settings.py
+++ talesome/talesome/settings.py
@@ -134,6 +134,7 @@ STATICFILES_DIRS = [
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+DEBUG = True
 CSRF_TRUSTED_ORIGINS = [
     'https://talesome-production.up.railway.app',
 ]
